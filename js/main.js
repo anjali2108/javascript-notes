ddbcd9e1551d73b6f5a22c74490a0b5d5a83b16e
anjali2108 <anjkesharwani2108@gmail.com>
2021-08-13 04:33:26 -0700

    javascript file

---
+++ js/main.js
@@ -0,0 +1,1 @@
+document.write("Ayush Kesarwani");
---
+++ js/set.js
@@ -0,0 +1,8 @@
+
+document.getElementById("a").innerHTML="<h1>Hello Gwalior</h1>";
+document.getElementById("b").innerHTML="<h1>Phone:795858857</h1>";
+document.getElementById("a").setAttribute("style","border:10px dotted red");
+document.getElementById("a").removeAttribute("style");
+
+
+document.querySelector(".c").innerHTML="<h1>Wow</h1>";
